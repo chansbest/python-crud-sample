b490bd43c4f22a7731c995824094ae2cd153b7d5
chans <chans.best@hotmail.com>
2020-08-17 02:00:22 +0530

    fix sqllite auto increment pk

--- sqllite.py
+++ sqllite.py
@@ -14,6 +14,25 @@ if os.path.isfile('covid.sqllite'):
 
 engine = create_engine('sqlite:///covid.sqllite',echo=False)
 sqlite_connection = engine.connect()
+meta = MetaData()
+
+print('creating table')
+us_states_table = Table(
+    "us_states",
+    meta,
+    Column("id", Integer, primary_key=True),
+    Column("state", String(50),nullable=False),
+    Column("record_date", DATE, nullable=False),
+    Column("fips", Integer, nullable=True),
+    Column("cases", Integer, nullable=True),
+    Column("deaths", Integer, nullable=True),
+    Column("confirmed_cases", Integer, nullable=True),
+    Column("confirmed_deaths", Integer, nullable=True),
+    Column("probable_cases", Integer, nullable=True),
+    Column("probable_deaths", Integer, nullable=True),
+    sqlite_autoincrement=True
+)
+meta.create_all(engine)
 
 print('reading csv')
 data = pd.read_csv('us-states.csv')
@@ -22,7 +41,7 @@ data = data.rename(columns={"date": "record_date"})
 print('writing to sqllite file')
 
 sqlite_table = "us_states"
-data.to_sql(sqlite_table, sqlite_connection, if_exists='fail',index_label='id')
+data.to_sql(sqlite_table, sqlite_connection,if_exists='append',index_label='id')
 print('done')
 
 
